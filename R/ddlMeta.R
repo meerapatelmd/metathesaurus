75fdf5018ef960c141c1372968017fba8a892c7f
Meera Patel <patelmeeray@gmail.com>
2020-08-30 16:11:27 -0400

    add full_ddl capability

--- R/ddlMeta.R
+++ R/ddlMeta.R
@@ -1,6 +1,5 @@
 #' DDL META Tables
 #' @param path Path to the unpacked RRF files
-#' @importFrom magrittr %>%
 #' @import preQL
 #' @importFrom DatabaseConnector dbExecute
 #' @export
@@ -9,17 +8,24 @@
 ddlMeta <-
         function(dbname = "umls",
                  username,
-                 password) {
+                 password,
+                 full = FALSE) {
+
 
                 conn <-
                 preQL::connectMySQL5.5(dbname = dbname,
                                        username = username,
                                        password = password)
 
-                sqlPath <- paste0(system.file(package = "setupMetathesaurus"), "/sql/ddl.sql")
+                if (full) {
+                        sqlPath <- paste0(system.file(package = "setupMetathesaurus"), "/sql/full_ddl.sql")
+
+                } else {
+                        sqlPath <- paste0(system.file(package = "setupMetathesaurus"), "/sql/ddl.sql")
+                }
+
                 executeSQL(sqlPath = sqlPath,
                            conn = conn)
-
                 preQL::dcMySQL5.5(conn = conn)
 
 
