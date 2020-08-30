5f1bce285561cb2104b91198ecee23a62a9fe11f
Meera Patel <patelmeeray@gmail.com>
2020-08-30 16:34:50 -0400

    remove loading of non-english tables

--- inst/sql/full_load.sql
+++ inst/sql/full_load.sql
@@ -34,55 +34,7 @@ load data local infile '@filePath/MRXNW_ENG.RRF' into table MRXNW_ENG fields ter
 
 load data local infile '@filePath/MRAUI.RRF' into table MRAUI fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
 
-load data local infile '@filePath/MRXW_BAQ.RRF' into table MRXW_BAQ fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_CHI.RRF' into table MRXW_CHI fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_CZE.RRF' into table MRXW_CZE fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_DAN.RRF' into table MRXW_DAN fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_DUT.RRF' into table MRXW_DUT fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
 load data local infile '@filePath/MRXW_ENG.RRF' into table MRXW_ENG fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_EST.RRF' into table MRXW_EST fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_FIN.RRF' into table MRXW_FIN fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_FRE.RRF' into table MRXW_FRE fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_GER.RRF' into table MRXW_GER fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_GRE.RRF' into table MRXW_GRE fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_HEB.RRF' into table MRXW_HEB fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_HUN.RRF' into table MRXW_HUN fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_ITA.RRF' into table MRXW_ITA fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_JPN.RRF' into table MRXW_JPN fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_KOR.RRF' into table MRXW_KOR fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_LAV.RRF' into table MRXW_LAV fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_NOR.RRF' into table MRXW_NOR fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_POL.RRF' into table MRXW_POL fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_POR.RRF' into table MRXW_POR fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_RUS.RRF' into table MRXW_RUS fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_SCR.RRF' into table MRXW_SCR fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_SPA.RRF' into table MRXW_SPA fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_SWE.RRF' into table MRXW_SWE fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
-
-load data local infile '@filePath/MRXW_TUR.RRF' into table MRXW_TUR fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
 
 load data local infile '@filePath/AMBIGSUI.RRF' into table AMBIGSUI fields terminated by '|' ESCAPED BY '' lines terminated by '\n';
 
