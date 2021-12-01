ac8226909833599cc6cfe05891175689fa6f28af
Meera <mpatel@concertai.com>
2021-12-01 15:38:43 -0500

    remove duplicate SQL

--- inst/sql/Hierarchy/02_plr_rxmap.sql
+++ inst/sql/Hierarchy/02_plr_rxmap.sql
@@ -21,34 +21,3 @@ mth_release_dt = mth_release_dt)
 
 
 SELECT setup_rxmap('{mth_version}', '{mth_release_dt}');
-
-
-
-
-WITH compl AS (
- SELECT tty,aui,code,str, in_aui AS ingr_aui FROM rxmap.rxnorm_ingredient_map
- UNION
- SELECT tty,aui,code,str, pin_aui AS ingr_aui FROM rxmap.rxnorm_precise_ingredient_map
- UNION
- SELECT tty,aui,code,str, min_aui AS ingr_aui FROM rxmap.rxnorm_multi_ingredient_map
-)
-
-select
-  map.tty,
-  map.aui,
-  map.code,
-  map.str,
-  ingr.*
-from rxclass.rxclass_rxnorm_in_pin_min_map ingr
-LEFT JOIN compl map
-ON map.ingr_aui = ingr.rxnorm_in_pin_min_aui
-where map.aui IS NOT NULL
-UNION
-select
-  ingr.rxnorm_in_pin_min_tty as tty,
-  ingr.rxnorm_in_pin_min_aui AS aui,
-  ingr.rxnorm_in_pin_min_code::integer AS code,
-  ingr.rxnorm_in_pin_min_str AS str,
-  ingr.*
-from rxclass.rxclass_rxnorm_in_pin_min_map ingr
-;
